4d0ec6134b93b5abe3d500fc7e29d5c31392a789
ZaenalRifai15 <zaenalrifai55@gmail.com>
2025-02-08 10:11:30 +0700

    Add files via upload

---
+++ Belajarjava1.java
@@ -0,0 +1,10 @@
+
+public class Belajarjava1 {
+
+
+    public static void main(String args[]) {
+       System.out.println("Nama Saya Zaenal RIfai Saepudin ");
+       System.out.println("nim 23013333");
+       
+    }
+}
---
+++ coba.java
@@ -0,0 +1,23 @@
+
+public class coba {
+
+    public static void main(String[] args) {
+        String nama = "Zaenal Rifai Saepudin";
+        int Umur = 19;
+        String Alamat = "Bandung";
+        String Jurusan = "Pendidikan Ilmu Komputer";
+        String Hoby = "Berdiam diri";
+        String Citacita = "Jadi Pemain Bola";
+        String Keterangan = "Saya Senang belajar pemrograman java";
+        
+        System.out.println("====My Biodata====");
+        System.out.println("Nama : " + nama);
+        System.out.println("Umur : " + Umur + "Tahun");
+        System.out.println("Alamat : " + Alamat);
+        System.out.println("Jurusan :" + Jurusan);
+        System.out.println("Hoby :" + Hoby);
+        System.out.println("Cita - Cita : " + Citacita);
+        System.out.println(Keterangan);
+    }
+}
+
